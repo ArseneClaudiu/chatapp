49e307f2a00864c467319b565d3d3895295115bb
Arsene Claudiu Ion <arsene.claudiu.ion@ibm.com>
2021-02-18 18:49:19 +0200

    some fixes

--- index.js
+++ index.js
@@ -144,3 +144,7 @@ app.post('/chatserver/userinfo/*', userInfo);
 app.listen(8080, function () {
 	console.log('Chat server listening on port 3000!')
 })
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
